bf961838cfeddae8b8b424c8fa0a09f658291daf
Amélia Alice <alice.cardoso10@gmail.com>
2022-09-12 21:10:12 -0300

    Update serverUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://employee-manager-back.herokuapp.com/actuator'
+  serverUrl: 'https://employee-manager-backend-production.up.railway.app/actuator'
 };
